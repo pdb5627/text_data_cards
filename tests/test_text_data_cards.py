ce16f27b7d9e967d5b81e9e8ff3e4f1c0f7d5a13
Paul Brown <pdb.lists@gmail.com>
2016-07-25 22:12:16 -0500

    Added tests for remaining DataCard classes.

--- tests/test_text_data_cards.py
+++ tests/test_text_data_cards.py
@@ -23,6 +23,7 @@ def tc():
                                      'DIAM', 'T', 'FIXED', 'RIGHT'],
                                     fixed_fields=(7, 8))
 
+
 @pytest.fixture()
 def tt_nomatch():
     #      I3 F5.4 F8.5    I2F8.5    F8.5    A8      A4
@@ -69,6 +70,7 @@ def tc_fixed_text():
 def tt_fixed_text_nomatch():
     return ['SOME WRONG TEXT']
 
+
 @pytest.fixture()
 def tt_fixed_text_match():
     return ['SOME FIXED TEXT']
@@ -83,5 +85,86 @@ def test_DataCardFixedText_nomatch(tc_fixed_text, tt_fixed_text_nomatch):
 
 
 # DataCardStack
+@pytest.fixture()
+def tc_stack():
+    return text_data_cards.DataCardStack([tc(), tc_fixed_text()])
+
+
+@pytest.fixture()
+def tt_stack_match():
+    return tt_match() + tt_fixed_text_match()
+
+
+@pytest.fixture()
+def tt_stack_nomatch1():
+    return tt_nomatch() + tt_fixed_text_match()
+
+
+@pytest.fixture()
+def tt_stack_nomatch2():
+    return tt_match() + tt_fixed_text_nomatch()
+
+
+def test_DataCardStack_match(tc_stack, tt_stack_match):
+    test_DataCard_match(tc_stack, tt_stack_match)
+
+
+def test_DataCardStack_nomatch1(tc_stack, tt_stack_nomatch1):
+    test_DataCard_nomatch(tc_stack, tt_stack_nomatch1)
+
+
+def test_DataCardStack_nomatch2(tc_stack, tt_stack_nomatch2):
+    test_DataCard_nomatch(tc_stack, tt_stack_nomatch2)
+
+
 # DataCardRepeat
-#
+@pytest.fixture()
+def tc_repeat():
+    return text_data_cards.DataCardRepeat(tc(), tc_fixed_text())
+
+
+@pytest.fixture()
+def tt_repeat_match():
+    return tt_match() + tt_match() + tt_fixed_text_match()
+
+
+@pytest.fixture()
+def tt_repeat_nomatch1():
+    return tt_nomatch() + tt_match() + tt_fixed_text_match()
+
+
+@pytest.fixture()
+def tt_repeat_nomatch2():
+    return tt_match() + tt_nomatch() + tt_fixed_text_match()
+
+
+@pytest.fixture()
+def tt_repeat_nomatch3():
+    return tt_match() + tt_match() + tt_fixed_text_nomatch()
+
+
+def test_DataCardRepeat_match(tc_repeat, tt_repeat_match):
+    assert tc_repeat.match(tt_repeat_match) is True
+    tc_repeat.read(tt_repeat_match)
+    for d in tc_repeat.data:
+        assert d['IP'] == 3
+        assert d['SKIN'] == 0.0
+        assert d['RESIS'] == 0.1357
+        assert d['IX'] == 0
+        assert d['REACT'] == 0.3959
+        assert d['DIAM'] == 1.18
+        assert d['T'] == 'TESTTEXT'
+        assert d['FIXED'] == 'FIXED'
+        assert d['RIGHT'] == 'RIGHT'
+
+
+def test_DataCardRepeat_nomatch1(tc_repeat, tt_repeat_nomatch1):
+    test_DataCard_nomatch(tc_repeat, tt_repeat_nomatch1)
+
+
+def test_DataCardRepeat_nomatch2(tc_repeat, tt_repeat_nomatch2):
+    test_DataCard_nomatch(tc_repeat, tt_repeat_nomatch2)
+
+
+def test_DataCardFixedText_nomatch3(tc_repeat, tt_repeat_nomatch2):
+    test_DataCard_nomatch(tc_repeat, tt_repeat_nomatch2)
