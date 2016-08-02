4ae28a8e61213cad0f0f2599aea269d8a9ba9e61
pdb5627 <pdbrown@prd.nppdnet.com>
2016-08-02 10:23:57 -0500

    Make pip -e installable

--- text_data_cards/__init__.py
+++ text_data_cards/__init__.py
@@ -3,3 +3,8 @@
 __author__ = 'Paul David Brown'
 __email__ = 'pdb.lists@gmail.com'
 __version__ = '0.1.0'
+
+from .text_data_cards import DataCard, DataCardFixedText, \
+    DataCardStack, DataCardRepeat
+
+__all__ = DataCard, DataCardFixedText, DataCardStack, DataCardRepeat
